03ba054510c076cb8b137abe3d8c76986a752799
Joshua Nichols <45806130+JoshuaAN@users.noreply.github.com>
2024-08-06 15:19:40 -0400

    Update optimization.h

--- optimization.h
+++ optimization.h
@@ -20,8 +20,23 @@ Eigen::SparseMatrix<double> NumericalJacobian(std::function<Eigen::VectorXd(Eige
                                               Eigen::VectorXd x, 
                                               double step);
 
+/**
+ * @brief Computes the skew symmetric matrix, ŵ, of the given vector w = [wˣ, wʸ, wᶻ]ᵀ.
+ *
+ *       [ 0  -wᶻ  wʸ]
+ *   ŵ = [ wᶻ  0  -wˣ]
+ *       [-wʸ  wˣ  0 ]
+ *
+ * @param w Input vector w = [wˣ, wʸ, wᶻ]ᵀ.
+ * @return Skew symmetric matrix ŵ.
+ */
 Eigen::Matrix3d SkewSymmetric(Eigen::Vector3d w);
 
+/**
+ * @brief Computes the matrix exponential of the skew symmetric matrix of the provided vector.
+ *
+ * We use the 
+ */
 Eigen::Matrix3d SkewSymmetricExponential(Eigen::Vector3d w);
 
 /**
